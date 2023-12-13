88fa01e3b3faa44661c6f484a98b6b2d90075b63
danielgbatista <daniel.g.batista21@outlook.com>
2023-12-12 21:36:19 -0300

    style: architecture organization of folder and changed message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Olá
+      Hello world!! This is run in localhost:3000.
     </main>
   );
 }
